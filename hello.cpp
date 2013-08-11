4762444f4c235b078455810092472322ca4b69db
zhu <zhouxiao1990@gmail.com>
2013-08-11 19:26:28 +0800

    GameBoard first time

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include "iostream"
-
+#include "string"
+#include "TransferFile.h"
 using namespace std;
 
-int main(){
-	cout<<"hello, github"<<endl;
+/*int main(){
+	
+	TransferFile tf;
+	string output = "J:/tools/tp/transfer/mmexport1318166272111_ctype_reverse.jpg";
+	string input = "J:/tools/tp/transfer/mmexport1318166272111.jpg.ctype";
+	tf.transfer(input, output);
 	system("pause");
-}
+}*/
